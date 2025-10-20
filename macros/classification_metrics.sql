905cda143fe3213cbb722f81e08ab54465dd308e
Matts52 <32623708+Matts52@users.noreply.github.com>
2025-10-19 20:23:24 -0700

    Add in classification counts to classification summary macro

--- macros/classification_metrics.sql
+++ macros/classification_metrics.sql
@@ -5,33 +5,39 @@
 {%- macro default__classification_metrics(actual, predicted, positive_label=1) -%}
 
 select
+    -- Classification counts
+    {{ dbt_ml_eval.true_positives(actual, predicted, positive_label) }} as true_positives,
+    {{ dbt_ml_eval.true_negatives(actual, predicted, positive_label) }} as true_negatives,
+    {{ dbt_ml_eval.false_positives(actual, predicted, positive_label) }} as false_positives,
+    {{ dbt_ml_eval.false_negatives(actual, predicted, positive_label) }} as false_negatives,
+
     -- Basic metrics
-    {{ accuracy(actual, predicted) }} as accuracy,
-    {{ balanced_accuracy(actual, predicted, positive_label) }} as balanced_accuracy,
-    {{ f1_score(actual, predicted, positive_label) }} as f1_score,
-    {{ precision(actual, predicted, positive_label) }} as precision,
-    {{ recall(actual, predicted, positive_label) }} as recall,
+    {{ dbt_ml_eval.accuracy(actual, predicted) }} as accuracy,
+    {{ dbt_ml_eval.balanced_accuracy(actual, predicted, positive_label) }} as balanced_accuracy,
+    {{ dbt_ml_eval.f1_score(actual, predicted, positive_label) }} as f1_score,
+    {{ dbt_ml_eval.precision(actual, predicted, positive_label) }} as precision,
+    {{ dbt_ml_eval.recall(actual, predicted, positive_label) }} as recall,
 
     -- Confusion matrix rates
-    {{ true_positive_rate(actual, predicted, positive_label) }} as true_positive_rate,
-    {{ true_negative_rate(actual, predicted, positive_label) }} as true_negative_rate,
-    {{ false_positive_rate(actual, predicted, positive_label) }} as false_positive_rate,
-    {{ false_negative_rate(actual, predicted, positive_label) }} as false_negative_rate,
-    {{ positive_predictive_value(actual, predicted, positive_label) }} as positive_predictive_value,
-    {{ negative_predictive_value(actual, predicted, positive_label) }} as negative_predictive_value,
-    {{ false_omission_rate(actual, predicted, positive_label) }} as false_omission_rate,
-    {{ false_discovery_rate(actual, predicted, positive_label) }} as false_discovery_rate,
+    {{ dbt_ml_eval.true_positive_rate(actual, predicted, positive_label) }} as true_positive_rate,
+    {{ dbt_ml_eval.true_negative_rate(actual, predicted, positive_label) }} as true_negative_rate,
+    {{ dbt_ml_eval.false_positive_rate(actual, predicted, positive_label) }} as false_positive_rate,
+    {{ dbt_ml_eval.false_negative_rate(actual, predicted, positive_label) }} as false_negative_rate,
+    {{ dbt_ml_eval.positive_predictive_value(actual, predicted, positive_label) }} as positive_predictive_value,
+    {{ dbt_ml_eval.negative_predictive_value(actual, predicted, positive_label) }} as negative_predictive_value,
+    {{ dbt_ml_eval.false_omission_rate(actual, predicted, positive_label) }} as false_omission_rate,
+    {{ dbt_ml_eval.false_discovery_rate(actual, predicted, positive_label) }} as false_discovery_rate,
 
     -- Derived metrics
-    {{ informedness(actual, predicted, positive_label) }} as informedness,
-    {{ markedness(actual, predicted, positive_label) }} as markedness,
-    {{ prevalence(actual, predicted, positive_label) }} as prevalence,
-    {{ prevalence_threshold(actual, predicted, positive_label) }} as prevalence_threshold,
-    {{ positive_likelihood_ratio(actual, predicted, positive_label) }} as positive_likelihood_ratio,
-    {{ negative_likelihood_ratio(actual, predicted, positive_label) }} as negative_likelihood_ratio,
-    {{ diagnostic_odds_ratio(actual, predicted, positive_label) }} as diagnostic_odds_ratio,
-    {{ fowlkes_mallows_index(actual, predicted, positive_label) }} as fowlkes_mallows_index,
-    {{ matthews_correlation(actual, predicted, positive_label) }} as matthews_correlation,
-    {{ jaccard_index(actual, predicted, positive_label) }} as jaccard_index
+    {{ dbt_ml_eval.informedness(actual, predicted, positive_label) }} as informedness,
+    {{ dbt_ml_eval.markedness(actual, predicted, positive_label) }} as markedness,
+    {{ dbt_ml_eval.prevalence(actual, predicted, positive_label) }} as prevalence,
+    {{ dbt_ml_eval.prevalence_threshold(actual, predicted, positive_label) }} as prevalence_threshold,
+    {{ dbt_ml_eval.positive_likelihood_ratio(actual, predicted, positive_label) }} as positive_likelihood_ratio,
+    {{ dbt_ml_eval.negative_likelihood_ratio(actual, predicted, positive_label) }} as negative_likelihood_ratio,
+    {{ dbt_ml_eval.diagnostic_odds_ratio(actual, predicted, positive_label) }} as diagnostic_odds_ratio,
+    {{ dbt_ml_eval.fowlkes_mallows_index(actual, predicted, positive_label) }} as fowlkes_mallows_index,
+    {{ dbt_ml_eval.matthews_correlation(actual, predicted, positive_label) }} as matthews_correlation,
+    {{ dbt_ml_eval.jaccard_index(actual, predicted, positive_label) }} as jaccard_index
 
 {%- endmacro -%}
